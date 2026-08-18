0db43e4e6042357cf471cc1767708a906025efd6
riansyah24 <jackrian72@gmail.com>
2026-08-18 23:31:08 +0700

    test 8

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,9 @@ function App() {
         const { level } = await battery.getBatteryLevel()
         const { isCharging } = await battery.isBatteryCharging()
         const { chargeType } = await battery.getChargeType()
-        setBatteryStats({level,isCharging,chargeType})
+        const { batteryTemperature } = await battery.getBatteryTemperature()
+        const { batteryVoltage } = await battery.getBatteryVoltage()
+        setBatteryStats({level,isCharging,chargeType, batteryTemperature, batteryVoltage})
       } catch (error) {
         console.error("Gagal mengambil level baterai:", error)
       }
@@ -28,6 +30,8 @@ function App() {
       <p>Battery level: {batteryStats.level}%</p>
       <p>Battery is charging: {`${batteryStats.isCharging}`}</p>
       <p>Charging type: {batteryStats.chargeType}</p>
+      <p>Battery temperature: {batteryStats.batteryTemperature}</p>
+      <p>Battery voltage: {batteryStats.batteryVoltage}V</p>
     </>
   )
 }
