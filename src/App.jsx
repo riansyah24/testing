2430a3b1e9b888c6f3c56de713e30119f0603822
riansyah24 <jackrian72@gmail.com>
2026-08-20 01:15:03 +0700

    feat: add vibrate

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,9 @@
-import { useState, useEffect } from 'react'
-import { deviceInfo } from "deviceinfo"
+import { Vibrate } from "vibrate"
 
-function App() {
-  const [info, setInfo] = useState("")
-
-  useEffect(() => {
-    const getDeviceInfo = async() => {
-      const { manufacture } = await deviceInfo.deviceInfoManufacture()
-      const { brand } = await deviceInfo.deviceInfoBrand()
-      const { board } = await deviceInfo.deviceInfoBoard()
-      const { hardware } = await deviceInfo.deviceInfoHardware()
-      const { osVersion } = await deviceInfo.deviceInfoOSVersion()
-      const { width } = await deviceInfo.deviceWidth()
-      const { height } = await deviceInfo.deviceHeight()
-      const { density } = await deviceInfo.deviceDensity()
-      const { totalRam } = await deviceInfo.deviceTotalRam()
-      const { availableRam } = await deviceInfo.deviceAvailableRam()
-
-      setInfo({manufacture,brand,board,hardware,osVersion,width,height,density,totalRam,availableRam})
-    }
-
-    getDeviceInfo()
-  },[])
-
-  return (
+export default function App() {
+  return(
     <>
-      <p>Manufacture : {info.manufacture}</p>
-      <p>Brand : {info.brand}</p>
-      <p>Board : {info.board}</p>
-      <p>Hardware : {info.hardware}</p>
-      <p>OS Version : {info.osVersion}</p>
-      <p>Width : {info.width}</p>
-      <p>Height : {info.height}</p>
-      <p>Density : {info.density}</p>
-      <p>Total RAM : {info.totalRam}</p>
-      <p>Available RAM : {info.availableRam}</p>
+      <button onClick={() => Vibrate.turnOnVibrate()}>Getar</button>
     </>
   )
 }
-
-export default App
