fab5f6c06b0276cb8af96f6666e5cf5719361a7e
riansyah24 <jackrian72@gmail.com>
2026-08-19 14:05:45 +0700

    feat: add manufacture

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,22 @@
 import { useState, useEffect } from 'react'
-import { battery } from "battery"
+import { deviceInfo } from "deviceinfo"
 
 function App() {
-  const [batteryStats, setBatteryStats] = useState({
-    level:0,
-    isCharging:false,
-    chargeType:"none", 
-    batteryTemperature:0,
-    batteryVoltage:0,
-    batteryCurrent:0,
-    batteryHealth:"none"
-  })
+  const [info, setInfo] = useState("")
 
   useEffect(() => {
-    const getBattery = async () => {
-      try {
-        const { level } = await battery.getBatteryLevel()
-        const { isCharging } = await battery.isBatteryCharging()
-        const { chargeType } = await battery.getChargeType()
-        const { batteryTemperature } = await battery.getBatteryTemperature()
-        const { batteryVoltage } = await battery.getBatteryVoltage()
-        const { batteryCurrent } = await battery.getBatteryCurrent()
-        const { batteryHealth } = await battery.getBatteryHealth()
-        setBatteryStats({level,isCharging,chargeType, batteryTemperature, batteryVoltage, batteryCurrent, batteryHealth})
-      } catch (error) {
-        console.error("Gagal mengambil level baterai:", error)
-      }
+    const getDeviceInfo = async() => {
+      const { manufacture } = await deviceInfo.deviceInfoManufacture()
+
+      setInfo(manufacture)
     }
 
-    getBattery()
-  }, [])
+    getDeviceInfo()
+  })
 
   return (
     <>
-      <p>Battery level: {batteryStats.level}%</p>
-      <p>Battery is charging: {`${batteryStats.isCharging}`}</p>
-      <p>Charging type: {batteryStats.chargeType}</p>
-      <p>Battery temperature: {batteryStats.batteryTemperature}</p>
-      <p>Battery voltage: {batteryStats.batteryVoltage}V</p>
-      <p>Battery current: {batteryStats.batteryCurrent}</p>
-      <p>Battery health: {batteryStats.batteryHealth}</p>
+      <p>{deviceInfo}</p>
     </>
   )
 }
