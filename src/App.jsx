39ec3ec2c9a5a9bf2263dc53c04b64bfbc8287d1
riansyah24 <jackrian72@gmail.com>
2026-08-19 18:48:40 +0700

    feat: add many feature

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,17 @@ function App() {
   useEffect(() => {
     const getDeviceInfo = async() => {
       const { manufacture } = await deviceInfo.deviceInfoManufacture()
+      const { brand } = await deviceInfo.deviceInfoBrand()
+      const { board } = await deviceInfo.deviceInfoBoard()
+      const { hardware } = await deviceInfo.deviceInfoHardware()
+      const { osVersion } = await deviceInfo.deviceInfoOSVersion()
+      const { width } = await deviceInfo.deviceWidth()
+      const { height } = await deviceInfo.deviceHeight()
+      const { density } = await deviceInfo.deviceDensity()
+      const { totalRam } = await deviceInfo.deviceTotalRam()
+      const { availableRam } = await deviceInfo.deviceAvailableRam()
 
-      setInfo(manufacture)
+      setInfo({manufacture,brand,board,hardware,osVersion,width,height,density,totalRam,avavailableRam})
     }
 
     getDeviceInfo()
@@ -16,7 +25,16 @@ function App() {
 
   return (
     <>
-      <p>{info}</p>
+      <p>Manufacture : {info.manufacture}</p>
+      <p>Brand : {info.brand}</p>
+      <p>Board : {info.board}</p>
+      <p>Hardware : {info.hardware}</p>
+      <p>OS Version : {info.osVersion}</p>
+      <p>Width : {info.width}</p>
+      <p>Height : {info.height}</p>
+      <p>Density : {info.density}</p>
+      <p>Total RAM : {info.totalRam}</p>
+      <p>Available RAM : {info.availableRam}</p>
     </>
   )
 }
